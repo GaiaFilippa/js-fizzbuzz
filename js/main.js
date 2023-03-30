291fcf1938641f299fd2623e4c647f397b838658
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-03-30 17:06:03 +0200

    added layout

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-let result = " ";
-
 for (let i = 1; i < 101; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+        console.log("FizzBuzz");
+        document.getElementById("container-for-squares").innerHTML += `
+    <div class="square">
+        <p>${"FizzBuzz"}</p>
+    </div>
+    `;
 
 
     } else if (i % 3 == 0) {
-    console.log("Fizz");
+        console.log("Fizz");
+        document.getElementById("container-for-squares").innerHTML += `
+    <div class="square">
+        <p>${"Fizz"}</p>
+    </div>
+    `;
 
     } else if (i % 5 == 0) {
-    console.log("Buzz");
+        console.log("Buzz");
+        document.getElementById("container-for-squares").innerHTML += `
+    <div class="square">
+        <p>${"Buzz"}</p>
+    </div>
+    `;
     
     } else {
         console.log(i);
+        document.getElementById("container-for-squares").innerHTML += `
+    <div class="square">
+        <p>${i}</p>
+    </div>
+    `;
     }
 }
